522d950273e0209760fe597ae2603d854f064c50
thanhvan1901 <you@example.comnvthanh190199@gmail.com>
2022-07-14 08:49:44 +0700

    editss1

--- ss1_csdl/baitap/bai1.sql
+++ ss1_csdl/baitap/bai1.sql
@@ -1,36 +1,28 @@
-
+drop database if exists ss1_baitap;
 create database ss1_baitap;
 use ss1_baitap;
 
-create table student(
-id_student int auto_increment,
-name_student varchar(55),
-date_of_birth date,
--- Id tự tăng
-primary key (id_student)
+CREATE TABLE class (
+    id_student INT AUTO_INCREMENT,
+    name_student VARCHAR(55),
+    PRIMARY KEY (id_student)
 );
--- tạo 1 dòng trong bảng 
-insert into student(name_student,date_of_birth) values ("thanh","1888-09-09"),
-("vinh","2002-01-01");
-
-select * from student ;
-
--- thêm trường "địa chỉ" vào bảng
-alter table student add dia_chi text ;
-
--- Thêm thôg tin mới vào table có thêm địa chỉ
-insert into student(name_student,date_of_birth,dia_chi) values("tuan","1997-01-30","Quang nam");
- 
-select *from student ;
-
--- thêm Đà Nẵng vào bản có tên thanh
-update student set dia_chi	 = "Đà Nẵng"
-where name_student = "thanh";
-
--- thêm huế vào bảng có tên vinh
-update student set dia_chi	 = "Huế"
-where name_student = "vinh";
-
+insert into class(name_student) values("Thanh"),("Vinh");
+SELECT 
+    *
+FROM
+    class;
+-- id, name, age, country
+CREATE TABLE teacher (
+    id_teacher INT AUTO_INCREMENT,
+    name_teacher VARCHAR(55),
+    age INT,
+    country VARCHAR(55),
+    PRIMARY KEY (id_teacher)
+); 
+insert into teacteacherher(name_teacher,age,country) values ("Nhân","45","Đà Nẵng"),
+("Hoàng","40","Huế");
+select * from teacher;
 
 
 
